137d50caa75d9609aad02db191633600627e299f
saniulnaim <saniulnaim@gmail.com>
2018-10-20 19:17:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => res.send('Hello World!' + Date()))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
  
+// to run this code :
 
+// powershell command : nodemon .\index.js
+// go to any browser
+// then write : http://localhost:3000/
  
  
